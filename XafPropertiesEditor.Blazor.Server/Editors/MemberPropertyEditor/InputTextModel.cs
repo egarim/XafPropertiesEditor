458d68af23412e178a0833c1704387c67d4bc37e
Jose Ojeda <joche.ojeda@bitframeworks.com>
2024-08-09 14:59:34 +0300

    Add project files.

---
+++ XafPropertiesEditor.Blazor.Server/Editors/MemberPropertyEditor/InputTextModel.cs
@@ -0,0 +1,64 @@
+﻿using DevExpress.ExpressApp.Blazor.Components.Models;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
+using System.Linq.Expressions;
+using DevExpress.ExpressApp.Blazor.Editors;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.BaseImpl;
+using XafPropertiesEditor.Module.BusinessObjects;
+
+namespace XafPropertiesEditor.Blazor.Server.Editors.MemberPropertyEditor
+{
+
+
+    [PropertyEditor(typeof(PropertyObject), true)]
+    public class CustomStringPropertyEditor : BlazorPropertyEditorBase
+    {
+        public CustomStringPropertyEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model) { }
+        public override InputTextModel ComponentModel => (InputTextModel)base.ComponentModel;
+        protected override IComponentModel CreateComponentModel()
+        {
+            
+            var model = new InputTextModel();
+            model.ValueExpression = () => model.Value;
+            model.ValueChanged = EventCallback.Factory.Create<PropertyObject>(this, value => {
+                model.Value = value;
+                OnControlValueChanged();
+                WriteValue();
+            });
+            return model;
+        }
+        protected override void ReadValueCore()
+        {
+            base.ReadValueCore();
+            ComponentModel.Value = (PropertyObject)PropertyValue;
+        }
+        protected override object GetControlValueCore() => ComponentModel.Value;
+        protected override void ApplyReadOnly()
+        {
+            base.ApplyReadOnly();
+            ComponentModel?.SetAttribute("readonly", !AllowEdit);
+        }
+    }
+    public class InputTextModel : ComponentModelBase
+    {
+
+        public PropertyObject Value
+        {
+            get => GetPropertyValue<PropertyObject>();
+            set => SetPropertyValue(value);
+        }
+        public EventCallback<PropertyObject> ValueChanged
+        {
+            get => GetPropertyValue<EventCallback<PropertyObject>>();
+            set => SetPropertyValue(value);
+        }
+        public Expression<Func<PropertyObject>> ValueExpression
+        {
+            get => GetPropertyValue<Expression<Func<PropertyObject>>>();
+            set => SetPropertyValue(value);
+        }
+        public override Type ComponentType => typeof(MemberComponent);
+    }
+}
---
+++ XafPropertiesEditor.Module/BusinessObjects/TestObject.cs
@@ -0,0 +1,112 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using static DevExpress.CodeParser.CodeStyle.Formatting.Rules.LineBreaks;
+
+namespace XafPropertiesEditor.Module.BusinessObjects
+{
+    public interface IDataType: INotifyPropertyChanged
+    {
+        Type DataType { get; set; }
+        string SelectedProperty { get; set; }
+    }
+   
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class TestObject : BaseObject,IXafEntityObject//, ICheckedListBoxItemsProvider, INotifyPropertyChanged
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public TestObject(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        public void OnCreated()
+        {
+            this.Member=new PropertyObject();
+            this.Member.Session = this.Session;
+            this.Member.PropertyChanged += Member_PropertyChanged;
+        }
+
+        private void Member_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            this.SelectedProperty= Member.SelectedProperty;
+        }
+        protected override void OnChanged(string propertyName, object oldValue, object newValue)
+        {
+            base.OnChanged(propertyName, oldValue, newValue);
+            if(propertyName == nameof(DataType))
+            {
+                if(!this.IsLoading && !this.IsSaving && this.Member!=null)
+                    this.Member.DataType = this.DataType;
+            }
+        }
+        void IXafEntityObject.OnSaving()
+        {
+            this.SelectedProperty = Member.SelectedProperty;
+        }
+
+        void IXafEntityObject.OnLoaded()
+        {
+            if(this.Member!=null)
+            {
+                this.Member.DataType = this.DataType;
+                this.Member.Session = this.Session;
+                this.Member.SelectedProperty = this.SelectedProperty;
+
+            }
+        }
+
+        [ValueConverter(typeof(TypeToStringConverter))]
+        [TypeConverter(typeof(LocalizedClassInfoTypeConverter))]
+        [Size(SizeAttribute.Unlimited)]
+        public Type DataType
+        {
+            get { return GetPropertyValue<Type>(nameof(DataType)); }
+            set { SetPropertyValue<Type>(nameof(DataType), value); }
+        }
+
+        PropertyObject member;
+        string selectedProperty;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string SelectedProperty
+        {
+            get => selectedProperty;
+            set => SetPropertyValue(nameof(SelectedProperty), ref selectedProperty, value);
+        }
+        
+        public PropertyObject Member
+        {
+            get => member;
+            set => SetPropertyValue(nameof(Member), ref member, value);
+        }
+        
+
+
+    }
+}
